50a3c54037dc53edf251158d597244fda64aa0a1
Chaiyapruk Parinyanuntakan <chaiyapruk6494@gmail.com>
2024-02-23 15:20:16 +0700

    add lab22.1

--- average.cpp
+++ average.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+#include <cstdlib>
 
+using namespace std;
+int main(int many, char *avg[] ){
+    float sum = 0, Avg = 0;
+    if (many == 1) printf("Please input numbers to find average.\n");
+    else {
+        for(int i = 0; i < many; i++){
+        sum += atoi(avg[i]);
+        if(i == many - 1) {
+            printf("---------------------------------\n");
+            Avg = sum/(many - 1);
+            printf("Average of %d numbers = %.0f \n", i, Avg);
+            printf("---------------------------------\n");
+            }
+        }
+    }
+}
